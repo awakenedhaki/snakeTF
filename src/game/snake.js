d08bb91bc157b925ffc5ee146b39c489a1fdf871
awakenedhaki <rodrigo.vallejosch@gmail.com>
2023-12-07 11:18:39 -0800

    Reorder Snake class methods

--- src/game/snake.js
+++ src/game/snake.js
@@ -16,43 +16,6 @@ class Snake {
   }
 
   /**
-   * Gets the head of the snake.
-   * @method Snake#head
-   * @returns {p5.Vector} The vector representing the head of the snake.
-   */
-  get head() {
-    return this.body[0];
-  }
-
-  /**
-   * Gets the tail of the snake.
-   * @method Snake#tail
-   * @returns {p5.Vector} The vector representing the tail of the snake.
-   */
-  get tail() {
-    return this.body[this.body.length - 1];
-  }
-
-  /**
-   * Extends the length of the snake by adding a new segment to its body.
-   * @method Snake#extendBody
-   */
-  extendBody() {
-    const tail = this.body[this.body.length - 1].copy();
-    this.body.push(tail);
-  }
-
-  /**
-   * Checks if the snake has eaten the food.
-   * @method Snake#eat
-   * @param {Food} food - The food object to check against.
-   * @returns {boolean} True if the snake's tail position matches the food position; otherwise, false.
-   */
-  eat(food) {
-    return this.head.equals(food.position);
-  }
-
-  /**
    * Changes the direction of the snake's movement.
    * @method Snake#changeDirection
    * @param {number} x - The x-component of the new velocity.
@@ -83,6 +46,25 @@ class Snake {
   }
 
   /**
+   * Checks if the snake has eaten the food.
+   * @method Snake#eat
+   * @param {Food} food - The food object to check against.
+   * @returns {boolean} True if the snake's tail position matches the food position; otherwise, false.
+   */
+  eat(food) {
+    return this.head.equals(food.position);
+  }
+
+  /**
+   * Extends the length of the snake by adding a new segment to its body.
+   * @method Snake#extendBody
+   */
+  extendBody() {
+    const tail = this.body[this.body.length - 1].copy();
+    this.body.push(tail);
+  }
+
+  /**
    * Updates the position and movement of the snake.
    * @method Snake#update
    */
@@ -107,5 +89,23 @@ class Snake {
       const segment = this.body[i];
       rect(segment.x, segment.y, tileSize, tileSize);
     }
+  }
+
+  /**
+   * Gets the head of the snake.
+   * @method Snake#head
+   * @returns {p5.Vector} The vector representing the head of the snake.
+   */
+  get head() {
+    return this.body[0];
+  }
+
+  /**
+   * Gets the tail of the snake.
+   * @method Snake#tail
+   * @returns {p5.Vector} The vector representing the tail of the snake.
+   */
+  get tail() {
+    return this.body[this.body.length - 1];
   }
 }
