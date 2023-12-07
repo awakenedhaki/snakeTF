4e63bc836a3f4bbccfa25fdd1bea41e621a1d957
awakenedhaki <rodrigo.vallejosch@gmail.com>
2023-12-07 12:30:26 -0800

    Implement GameInstance and GameInstance collection classes

---
+++ src/managers/gameInstancesManager.js
@@ -0,0 +1,40 @@
+/**
+ * Manages multiple game instances.
+ */
+class GameInstanceManager {
+  constructor(nInstances) {
+    this.nInstances = nInstances;
+    this.gameInstances = [];
+  }
+
+  /**
+   * Creates a new game instance.
+   * @returns {GameInstance} The created game instance.
+   */
+  createGameInstance() {
+    return new GameInstance();
+  }
+
+  /**
+   * Creates multiple game instances.
+   */
+  createGameInstances() {
+    for (let i = 0; i < this.nInstances; i++) {
+      this.GameInstances.push(this.createGameInstance());
+    }
+  }
+
+  /**
+   * Updates all game instances.
+   */
+  update() {}
+
+  /**
+   * Displays all game instances.
+   */
+  show() {}
+
+  get snakes() {
+    return this.gameInstances.map((gameInstance) => gameInstance.snake);
+  }
+}
