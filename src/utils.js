c342fa361dc40e2554c3150244b9289b6ac7ac35
awakenedhaki <rodrigo.vallejosch@gmail.com>
2023-12-07 11:28:01 -0800

    Scale manhattan distance by tile size

--- src/utils.js
+++ src/utils.js
@@ -13,11 +13,11 @@ function generate2DCoordinates() {
 }
 
 /**
- * Calculates the Manhattan distance between the snake's head and the food.
+ * Calculates the Manhattan distance between the snake's head and the food in terms of tiles.
  * @param {Snake} snake - The snake object.
  * @param {Food} food - The food object.
- * @returns {number} The Manhattan distance between the snake's head and the food.
+ * @returns {number} The Manhattan distance between the snake's head and the food in terms of tiles..
  */
 function manhattanDistance(snake, food) {
-  return abs(snake.head.x - food.x) + abs(snake.head.y - food.y);
+  return (abs(snake.head.x - food.x) + abs(snake.head.y - food.y)) / tileSize;
 }
