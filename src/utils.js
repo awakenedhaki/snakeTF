70cc7176d217f288d69036f74fbe3dc1a09d8ecc
awakenedhaki <rodrigo.vallejosch@gmail.com>
2023-12-07 11:25:57 -0800

    Implement manhattan distance function

--- src/utils.js
+++ src/utils.js
@@ -11,3 +11,13 @@ function generate2DCoordinates() {
 
   return createVector(x, y);
 }
+
+/**
+ * Calculates the Manhattan distance between the snake's head and the food.
+ * @param {Snake} snake - The snake object.
+ * @param {Food} food - The food object.
+ * @returns {number} The Manhattan distance between the snake's head and the food.
+ */
+function manhattanDistance(snake, food) {
+  return abs(snake.head.x - food.x) + abs(snake.head.y - food.y);
+}
