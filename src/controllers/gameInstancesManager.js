5bfb9ba7a063d3ce1ef3be7798bda781708ba280
awakenedhaki <rodrigo.vallejosch@gmail.com>
2023-12-08 00:20:34 -0800

    Implement selection and mutation

--- src/controllers/gameInstancesManager.js
+++ src/controllers/gameInstancesManager.js
@@ -41,6 +41,64 @@ class GameInstancesManager {
   }
 
   /**
+   * Creates the next generation of game instances by performing selection and
+   * mutation.
+   * @returns {void}
+   */
+  nextGeneration() {
+    this.selection();
+    this.mutation();
+  }
+
+  /**
+   * Performs selection on the game instances based on their fitness.
+   * This method calculates the fitness of each game instance, normalizes the
+   * fitness scores, and then selects a new set of game instances based on these
+   * normalized fitness scores.
+   * @returns {void}
+   */
+  selection() {
+    const fitnesses = this.gameInstances.map((gameInstance) =>
+      gameInstance.calculateFitness()
+    );
+
+    const totalFitness = fitnesses.reduce((acc, fitness) => acc + fitness, 0);
+    const fitnessesNormalized = fitnesses.map(
+      (fitness) => fitness / totalFitness
+    );
+
+    const newGameInstances = [];
+    for (let i = 0; i < this.nInstances; i++) {
+      const gameInstance = weightedRandomSelection(
+        this.gameInstances,
+        fitnessesNormalized
+      );
+
+      gameInstance.reset();
+
+      newGameInstances.push(gameInstance);
+    }
+
+    this.gameInstances = newGameInstances;
+  }
+
+  /**
+   * Performs mutation on the game instances.
+   * @returns {void}
+   */
+  mutation() {
+    this.gameInstances.forEach((gameInstance) => gameInstance.mutate());
+  }
+
+  /**
+   * Checks if all game instances are over.
+   * @returns {boolean} - True if all game instances are over, false otherwise.
+   */
+  allInstancesOver() {
+    return this.gameInstances.every((gameInstance) => gameInstance.isOver);
+  }
+
+  /**
    * Runs all game instances managed by this game instances manager.
    * @returns {void}
    */
