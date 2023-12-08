9b17527cdb656f2ab63ad4edb15bfc32591183d4
awakenedhaki <rodrigo.vallejosch@gmail.com>
2023-12-07 16:05:24 -0800

    Move all game management logic to the GameInstance class

--- sketch.js
+++ sketch.js
@@ -1,5 +1,4 @@
 // Declaring Global Variables ==================================================
-let snake, food;
 
 // Setting Constants ===========================================================
 const tileSize = 10;
@@ -9,36 +8,11 @@ function setup() {
   createCanvas(600, 600);
   frameRate(10);
 
-  snake = new Snake(300, 300);
-  food = new Food();
+  gameInstance = new GameInstance();
 }
 
 function draw() {
-  background(59, 37, 44);
-
-  // Game Loop
-  food.show();
-  if (snake.eat(food)) {
-    food.updateLocation();
-    snake.extendBody();
-  }
-  snake.update();
-  snake.show();
-
-  // Terminate Game
-  if (gameOver(snake)) {
-    translate(width / 2, height / 2);
-    textAlign(CENTER, CENTER);
-
-    fill(250, 128, 114);
-    textSize(32);
-    text("Game Over.", 0, 0);
-
-    fill(255, 128, 114);
-    textSize(24);
-    text("Score: " + (snake.body.length - 1), 0, 25);
-    noLoop();
-  }
+  gameInstance.run();
 }
 
 function keyPressed() {
